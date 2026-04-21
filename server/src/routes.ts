97e25ead2d08f7e5263e395a5d446e359754ed95
Luke B <luke.brenn04@gmail.com>
2026-04-21 13:07:26 -0400

    Add stopRouter to vehicles route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import vehicleRouter from "./models/cart/cart.route";
+import stopRouter from "./models/cart/stop.route";
 
 const routes = Router();
 
 // Defines the base paths and the routers that are used
 routes.use("/vehicles", vehicleRouter);
+routes.use("/vehicles", stopRouter);
 
 export default routes;
