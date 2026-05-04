efaba203befb385e9b6741e01ae78e2a858c7242
jake <dinhjx@dukes.jmu.edu>
2026-05-04 16:42:57 -0400

    Properly communicating with anomaly messages

--- server/src/models/cart/ros.ts
+++ server/src/models/cart/ros.ts
@@ -134,7 +134,7 @@ const CART_TOPICS = {
     throttle_rate: 500, // this can be changed based on bandwidth
   },
   anomaly_result: {
-    name: "ai_anomaly_logging_ui", // "/aad/alerts",
+    name: "/aad/alerts",
     messageType: "std_msgs/msg/String",
     throttle_rate: 500,
   },
